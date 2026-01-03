572252d275a6c9fc56f580827cc3ddf459e2ad54
KirillovAV1 <a.vl.kirillov@gmail.com>
2026-01-03 14:58:57 +0300

    Ручки get/post/delete

--- main.py
+++ main.py
@@ -1,7 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Body
+import uvicorn
 
 app = FastAPI()
 
+hotels = [
+    {"id": 1, "title": "Sochi"},
+    {"id": 2, "title": "Дубай"},
+]
+
+
 @app.get("/hotels")
-def func():
-    return "Hello World"
+def get_hotels(
+        id: int | None = Query(None, description="Айдишник"),
+        title: str | None = Query(None, description="Название отеля"),
+):
+    hotels_ = []
+    for hotel in hotels:
+        if id and hotel["id"] != id:
+            continue
+        if title and hotel["title"] != title:
+            continue
+        hotels_.append(hotel)
+    return hotels_
+
+
+@app.post("/hotels")
+def create_hotel(
+        title: str = Body(embed=True),
+):
+    global hotels
+    hotels.append({
+        "id": hotels[-1]["id"] + 1,
+        "title": title
+    })
+    return {"status": "OK"}
+
+
+@app.delete("/hotels/{hotel_id}")
+def delete_hotel(hotel_id: int):
+    global hotels
+    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+    return {"status": "OK"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
