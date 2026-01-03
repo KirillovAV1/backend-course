da1aa6574c96bf564d0394812fa1490cf385f991
KirillovAV1 <a.vl.kirillov@gmail.com>
2026-01-03 15:15:17 +0300

    Задание №1: PUT и PATCH ручки отелей

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import uvicorn
 app = FastAPI()
 
 hotels = [
-    {"id": 1, "title": "Sochi"},
-    {"id": 2, "title": "Дубай"},
+    {"id": 1, "title": "Sochi", "name": "sochi"},
+    {"id": 2, "title": "Дубай", "name": "dubai"},
 ]
 
 
@@ -13,12 +13,15 @@ hotels = [
 def get_hotels(
         id: int | None = Query(None, description="Айдишник"),
         title: str | None = Query(None, description="Название отеля"),
+        name: str | None = Query(None, description="Какой-то еще идентификатор"),
 ):
     hotels_ = []
     for hotel in hotels:
         if id and hotel["id"] != id:
             continue
         if title and hotel["title"] != title:
+            continue
+        if name and hotel["name"] != name:
             continue
         hotels_.append(hotel)
     return hotels_
@@ -27,12 +30,46 @@ def get_hotels(
 @app.post("/hotels")
 def create_hotel(
         title: str = Body(embed=True),
+        name: str = Body(embed=True),
 ):
     global hotels
     hotels.append({
         "id": hotels[-1]["id"] + 1,
-        "title": title
+        "title": title,
+        "name": name
     })
+    return {"status": "OK"}
+
+
+@app.put("/hotels/{id}")
+def full_update_hotel(
+        id: int,
+        title: str = Body(embed=True),
+        name: str = Body(embed=True),
+):
+    global hotels
+    for hotel in hotels:
+        if hotel["id"] == id:
+            hotel["title"] = title
+            hotel["name"] = name
+            break
+    return {"status": "OK"}
+
+
+@app.patch("/hotels/{id}")
+def partial_update_hotel(
+        id: int,
+        title: str | None = Body(None, embed=True),
+        name: str | None = Body(None, embed=True),
+):
+    global hotels
+    for hotel in hotels:
+        if hotel["id"] == id:
+            if title:
+                hotel["title"] = title
+            if name:
+                hotel["name"] = name
+            break
     return {"status": "OK"}
 
 
