e1ca9e49070be8644d3953be1d8c3c3ab3158969
KirillovAV1 <a.vl.kirillov@gmail.com>
2026-01-12 10:44:42 +0300

    Создание database.py, добавление DB_URL в config

--- src/config.py
+++ src/config.py
@@ -1,10 +1,20 @@
+from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 class Settings(BaseSettings):
     DB_NAME: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
 
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    @property
+    def DB_URL(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file=BASE_DIR / ".env", extra="ignore")
 
 
 settings = Settings()
