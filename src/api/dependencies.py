2c3994d984d0e9e4c21d3446ca4a34b555e9c211
KirillovAV1 <a.vl.kirillov@gmail.com>
2026-01-17 22:24:59 +0300

    Small fix

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -3,8 +3,8 @@ from pydantic import BaseModel
 from typing import Annotated
 
 class PaginationParams(BaseModel):
-    page: Annotated[int, Query(1, ge=1, description="Номер страницы для пагинации")]
-    per_page: Annotated[int, Query(None, ge=1, le=100, description="Количество отелей на странице")]
+    page: Annotated[int | None, Query(1, ge=1, description="Номер страницы для пагинации")]
+    per_page: Annotated[int | None, Query(None, ge=1, le=100, description="Количество отелей на странице")]
 
 
 PaginationDep = Annotated[PaginationParams, Depends()]
