73421e5fc94d982ab5e136149f76558e853e0b5d
KirillovAV1 <a.vl.kirillov@gmail.com>
2026-01-19 09:33:00 +0300

    add POST /auth/register; hashed_password not ready yet

---
+++ src/api/auth.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, Body
+from fastapi.openapi.models import Example
+
+from src.schemas.users import UserRequestAdd, UserAdd
+from src.database import async_session_maker
+from src.repositories.users import UsersRepository
+
+router = APIRouter(prefix="/auth",
+                   tags=["Авторизация и аутентификация"])
+
+
+@router.post("/register",
+             summary="Регистрация пользователя")
+async def register_user(user_data: UserRequestAdd = Body(openapi_examples={
+    "user1": Example(
+        summary="Пример 1",
+        value={"email": "test_max@gmail.com",
+               "password": "qwerty123"}
+    ),
+    "user2": Example(
+        summary="Пример 2",
+        value={"email": "user_mail@yandex.com",
+               "password": "passw0rd"}
+    )
+})):
+    hashed_password = "dsa21xcz"
+    new_user_data = UserAdd(email=user_data.email,
+                            hashed_password=hashed_password)
+    async with async_session_maker() as session:
+        await UsersRepository(session).add(new_user_data)
+        await session.commit()
+        return {"status": "ok"}
+
+
+
---
+++ src/schemas/users.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, Field, EmailStr
+
+
+class UserRequestAdd(BaseModel):
+    email: EmailStr = Field(description="Электронный адрес")
+    password: str = Field(description="Пароль")
+
+
+class UserParams(BaseModel):
+    email: EmailStr = Field(description="Электронный адрес")
+
+    phone_number: str | None = Field(default=None, description="Номер телефона")
+    first_name: str | None = Field(default=None, description="Имя пользователя")
+    last_name: str | None = Field(default=None, description="Фамилия пользователя")
+    username: str | None = Field(default=None, description="Никнейм пользователя")
+
+
+class UserAdd(UserParams):
+    hashed_password: str = Field(description="Хеш-пароля")
+
+
+class User(UserParams):
+    id: int = Field(description="ID пользователя")
